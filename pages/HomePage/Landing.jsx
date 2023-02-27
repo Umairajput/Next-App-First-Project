f82e3da5e3b1240b00ffc9ddf208305ee99c39df
Zain_Ali_Sidd <114559080+Zain-Ali-Siddiqui@users.noreply.github.com>
2023-02-27 19:41:23 +0500

    update

--- pages/HomePage/Landing.jsx
+++ pages/HomePage/Landing.jsx
@@ -50,9 +50,9 @@ export default function LandingPage() {
                 <Image src="" alt="" />
               </div>
               <div className="floating-div">
-                <div className="watch-1"><Image src={pic1}  /></div>
-                <div className="watch-2"><Image src={pic2}/></div>
-                <div className="watch-3"><Image src={pic3}/></div>
+                <div className="watch-1"><Image src={pic1} className='img' /></div>
+                <div className="watch-2"><Image src={pic2} className='img' /></div>
+                <div className="watch-3"><Image src={pic3} className='img' /></div>
               </div>
 
             </div>
