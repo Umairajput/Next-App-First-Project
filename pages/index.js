28e97293ef2e404b10f987b7f3612f8a6bed3c67
Umair Rajput <107687680+Umairajput@users.noreply.github.com>
2023-02-27 18:43:43 +0500

    updated

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
 import { Inter } from 'next/font/google'
-import Navbar from './NavBar'
 import LandingPage from './HomePage/Landing'
 
 const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <>
-      <Navbar />
       <LandingPage />
     </>
   )
