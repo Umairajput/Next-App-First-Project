8b023b7a293463302b48f00765b97bea2e4c7d5e
Zain_Ali_Sidd <114559080+Zain-Ali-Siddiqui@users.noreply.github.com>
2023-02-27 19:20:29 +0500

    update

--- pages/HomePage/Landing.jsx
+++ pages/HomePage/Landing.jsx
@@ -4,98 +4,90 @@ import Image from "next/image"
 export default function LandingPage() {
   let product = [
     {
-      img: logo,
-      Product: 'name',
-      price: '200$',
-      button: 'Add To Cart'
+      img1: logo,
+      img2: logo,
+      img3: logo,
+      Product: 'Samsung watch',
+      about: 'Combine smart and beauty',
+      price: '199$',
+      button: 'BUY NOW'
     },
     {
-      img: logo,
-      Product: 'name',
-      price: '200$',
-      button: 'Add To Cart'
+      img1: logo,
+      img2: logo,
+      img3: logo,
+      Product: 'Samsung watch',
+      about: 'Combine smart and beauty',
+      price: '199$',
+      button: 'BUY NOW'
     },
     {
-      img: logo,
-      Product: 'name',
-      price: '200$',
-      button: 'Add To Cart'
+      img1: logo,
+      img2: logo,
+      img3: logo,
+      Product: 'Samsung watch',
+      about: 'Combine smart and beauty',
+      price: '199$',
+      button: 'BUY NOW'
     },
-    {
-      img: logo,
-      Product: 'name',
-      price: '200$',
-      button: 'Add To Cart'
-    },
-
-
 
   ]
   return (
     <>
       {/* <Slider /> */}
-      <main>
-        {product?.map((v, i) => {
-          return (
-            <>
-              <div class="product">
-                <Image src={v?.img} alt="Product 1" />
-                <h2>{v?.Product}</h2>
-                <p>{v?.price}</p>
-                <button>{v?.button}</button>
-              </div>
-            </>
-          )
-        })
-        }
-        <div class="container">
+      <div className="container">
+          {product?.map((v, i) => {
+            return (
+              <>
+              <div className="card-container">
+                <input type="radio" name="colors-btn" id="color-1" checked />
+                <input type="radio" name="colors-btn" id="color-2" />
+                <input type="radio" name="colors-btn" id="color-3" />
+                <div className="watch-area">
 
-          <div class="card-container">
-            <input type="radio" name="colors-btn" id="color-1" checked />
-            <input type="radio" name="colors-btn" id="color-2" />
-            <input type="radio" name="colors-btn" id="color-3" />
-            <div class="watch-area">
+                  <div className="logo-div">
+                    <Image src="" alt="" />
+                  </div>
+                  <div className="floating-div">
+                    <div className="watch-1"><Image src={v?.img1} /></div>
+                    <div className="watch-2"><Image src={v?.img2} /></div>
+                    <div className="watch-3"><Image src={v?.img3} /></div>
+                  </div>
 
-              <div class="logo-div">
-                <Image src="" alt="" />
-              </div>
-              <div class="floating-div">
-                <div class="watch-1"><Image src={logo} alt="samsung purple" /></div>
-                <div class="watch-2"><Image src={logo} width="" height="" alt="samsung pink" /></div>
-                <div class="watch-3"><Image src={logo} width="" height="" alt="samsung black" /></div>
-              </div>
-
-            </div>
-            <div class="text-area">
-              <div class="heading-area">
-                <h2>Samsung watch</h2>
-                <h4>Combine smart and beauty</h4>
-              </div>
-
-              <p class="paragraph-area">
-                Monitor your health around the clock even at night.
-                Achieve your wellness goal by measuring body composition with Samsung BioActive sensor.
-              </p>
-
-              <div class="color-selection">
-                <div class="h5-text"><h5>Available Colors: </h5></div>
-
-                <div class="color-pick-div">
-                  <label for="color-1" class="colors color-1"></label>
-                  <label for="color-2" class="colors color-2"></label>
-                  <label for="color-3" class="colors color-3" ></label>
                 </div>
-              </div>
-              <div class="price-and-buy-btn">
-                <h2 class="price-1">€ 279,-</h2>
-                <h2 class="price-2">€ 279,-</h2>
-                <h2 class="price-3">€ 279,-</h2>
-                <button class="buy-btn">BUY NOW</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </main>
+                <div className="text-area">
+                  <div className="heading-area">
+                    <h2>{v?.Product}</h2>
+                    <h4>{v?.about}</h4>
+                  </div>
+
+                  <p className="paragraph-area">
+                    Monitor your health around the clock even at night.
+                    Achieve your wellness goal by measuring body composition with Samsung BioActive sensor.
+                  </p>
+
+                  <div className="color-selection">
+                    <div className="h5-text"><h5>Select Colors: </h5></div>
+
+                    <div className="color-pick-div">
+                      <label for="color-1" className="colors color-1"></label>
+                      <label for="color-2" className="colors color-2"></label>
+                      <label for="color-3" className="colors color-3" ></label>
+                    </div>
+                  </div>
+                  <div className="price-and-buy-btn">
+                    <h2 className="price-1">{v?.price},-</h2>
+                    <h2 className="price-2">{v?.price},-</h2>
+                    <h2 className="price-3">{v?.price},-</h2>
+                    <button className="buy-btn">{v?.button}</button>
+                  </div>
+                </div>
+                </div>
+              </>
+            )
+          })
+          }
+      </div>
     </>
   )
 }
